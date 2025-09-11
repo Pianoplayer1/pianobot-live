3d7b788b3f2c0ff52ec5b49aab014ec2fce90d3a
Pianoplayer1 <pianoplayer0001@gmail.com>
2025-09-11 17:42:51 +0200

    edit territory tracking to track everything instead of selected

--- pianobot/db/territories.py
+++ pianobot/db/territories.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
 from pianobot.db import Connection
 
 
 class Territory:
-    def __init__(self, name: str, guild: str) -> None:
+    def __init__(self, name: str, guild: str, acquired: datetime) -> None:
         self._name = name
         self._guild = guild
+        self._acquired = acquired
 
     @property
     def name(self) -> str:
@@ -14,22 +17,26 @@ class Territory:
     def guild(self) -> str:
         return self._guild
 
+    @property
+    def acquired(self) -> datetime:
+        return self._acquired
+
 
 class TerritoryTable:
     def __init__(self, con: Connection) -> None:
         self._con = con
 
-    async def add(self, name: str, guild: str | None) -> None:
+    async def add(self, name: str, guild: str | None, acquired: datetime) -> None:
         await self._con.execute(
-            'INSERT INTO territories VALUES ($1, $2) ON CONFLICT (name) DO NOTHING', name, guild
+            'INSERT INTO territories VALUES ($1, $2, $3) ON CONFLICT (name) DO NOTHING', name, guild, acquired
         )
 
     async def get_all(self) -> list[Territory]:
-        result = await self._con.query('SELECT name, guild FROM territories')
-        return [Territory(row[0], row[1]) for row in result]
+        result = await self._con.query('SELECT name, guild, acquired FROM territories')
+        return [Territory(row[0], row[1], row[2]) for row in result]
 
     async def remove(self, name: str) -> None:
         await self._con.execute('DELETE FROM territories WHERE name = $1', name)
 
-    async def update(self, name: str, guild: str | None) -> None:
-        await self._con.execute('UPDATE territories SET guild = $1 WHERE name = $2', guild, name)
+    async def update(self, name: str, guild: str | None, acquired: datetime) -> None:
+        await self._con.execute('UPDATE territories SET guild = $1, acquired = $2 WHERE name = $3', guild, acquired, name)
--- pianobot/tasks/territories.py
+++ pianobot/tasks/territories.py
@@ -5,7 +5,7 @@ from logging import getLogger
 from typing import TYPE_CHECKING
 
 from corkus.errors import CorkusException
-from discord import TextChannel
+from discord import Embed, TextChannel
 
 if TYPE_CHECKING:
     from pianobot import Pianobot
@@ -13,45 +13,12 @@ if TYPE_CHECKING:
 
 async def territories(bot: Pianobot) -> None:
     db_terrs = {terr.name: terr for terr in await bot.database.territories.get_all()}
-    notify = None
-    missing = []
+
     try:
         wynn_territories = await bot.corkus.territory.list_all()
     except CorkusException as e:
         getLogger('tasks.territories').warning('Error when fetching list of territories: %s', e)
         return
-
-    for territory in wynn_territories:
-        guild_name = None if territory.guild is None else territory.guild.name
-        if territory.name not in db_terrs.keys():
-            continue
-        if db_terrs[territory.name].guild != guild_name:
-            await bot.database.territories.update(territory.name, guild_name)
-        if guild_name != 'Eden':
-            missing.append(territory)
-            if db_terrs[territory.name].guild == 'Eden' and territory.guild is not None:
-                notify = territory
-    if len(missing) == 0 and any(terr.guild != 'Eden' for terr in db_terrs.values()):
-        for server in await bot.database.servers.get_all():
-            if server.territory_log_channel is not None:
-                channel = bot.get_channel(server.territory_log_channel)
-                if isinstance(channel, TextChannel):
-                    await channel.send('Fully reclaimed!')
-                else:
-                    getLogger('tasks.territories').warning(
-                        'Channel %s not found', server.territory_log_channel
-                    )
-    if notify is None:
-        return
-
-    terrs_msg = '\n'.join([f'- {terr.name} ({terr.guild.name})' for terr in missing][:10])
-    if len(missing) > 10:
-        terrs_msg += f'\n- ... ({len(missing) - 10} more)'
-    msg = (
-        f'{notify.guild.name if notify.guild is not None else None} has taken control of'
-        f' {notify.name}! ({len(missing)} missing)'
-        # ```All missing territories ({len(missing)}):\n\n{terrs_msg}```'
-    )
 
     try:
         eden = await bot.corkus.guild.get('Eden')
@@ -65,26 +32,66 @@ async def territories(bot: Pianobot) -> None:
         default=-1,
     )
 
-    for server in await bot.database.servers.get_all():
-        if server.territory_log_channel is None:
+    async def send_territory_message(terr_name: str, old_guild: str, new_guild: str, time_held: timedelta) -> None:
+        value = time_held.days + (time_held.seconds / 86400)
+        unit = 'day'
+        if value < 3:
+            value *= 24
+            unit = 'hour'
+            if value < 1:
+                value *= 60
+                unit = 'minute'
+        value = round(value)
+        if value != 1:
+            unit += 's'
+
+        old_count = sum(1 for t in wynn_territories if t.guild and t.guild.name == old_guild)
+        new_count = sum(1 for t in wynn_territories if t.guild and t.guild.name == new_guild)
+
+        embed = Embed(
+            color=0x00aa00 if new_guild == 'Eden' else 0xaa0000,
+            title=':crossed_swords:   Territory ' + ('captured' if new_guild == 'Eden' else 'lost'),
+            description=f"{old_guild} ({old_count})\n:arrow_forward:  {new_guild} ({new_count})",
+        )
+        embed.set_author(name=terr_name)
+        embed.set_footer(text=f'Held for {value} {unit}')
+
+        for server in await bot.database.servers.get_all():
+            if server.territory_log_channel is None:
+                continue
+            if (
+                    server.ping_interval is not None
+                    and server.ping_role is not None
+                    and datetime.now(timezone.utc)
+                    >= (server.last_ping or datetime.min.replace(tzinfo=timezone.utc))
+                    + timedelta(minutes=server.ping_interval)
+                    and (6 if server.ping_rank is None else server.ping_rank) > highest_rank
+            ):
+                ping = f'<@&{server.ping_role}>'
+                await bot.database.servers.update_last_ping(
+                    server.server_id, datetime.now(timezone.utc)
+                )
+            else:
+                ping = None
+            channel = bot.get_channel(server.territory_log_channel)
+            if isinstance(channel, TextChannel):
+                await channel.send(ping, embed=embed)
+            else:
+                getLogger('tasks.territories').warning(
+                    'Channel %s not found', server.territory_log_channel
+                )
+
+    for territory in wynn_territories:
+        guild_name = None if territory.guild is None else territory.guild.name
+        if territory.name not in db_terrs.keys():
+            await bot.database.territories.add(territory, guild_name, territory.acquired)
             continue
-        temp_msg = msg
-        if (
-            server.ping_interval is not None
-            and server.ping_role is not None
-            and datetime.now(timezone.utc)
-            >= (server.last_ping or datetime.min.replace(tzinfo=timezone.utc))
-            + timedelta(minutes=server.ping_interval)
-            and (6 if server.ping_rank is None else server.ping_rank) > highest_rank
-        ):
-            temp_msg = f'<@&{server.ping_role}>\n{msg}'
-            await bot.database.servers.update_last_ping(
-                server.server_id, datetime.now(timezone.utc)
-            )
-        channel = bot.get_channel(server.territory_log_channel)
-        if isinstance(channel, TextChannel):
-            await channel.send(temp_msg)
-        else:
-            getLogger('tasks.territories').warning(
-                'Channel %s not found', server.territory_log_channel
-            )
+        if db_terrs[territory.name].guild != guild_name:
+            await bot.database.territories.update(territory.name, guild_name, territory.acquired)
+            if db_terrs[territory.name].guild == 'Eden' or guild_name == 'Eden':
+                await send_territory_message(
+                    territory.name,
+                    db_terrs[territory.name].guild,
+                    guild_name,
+                    territory.acquired - db_terrs[territory.name].acquired
+                )
